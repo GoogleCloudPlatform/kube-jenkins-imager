55307cb76821637a922a3d2ef0f3d31dfe6b1c05
Evan Brown <evanbrown@google.com>
2015-07-15 15:29:14 -0700

    e2e test will override container images if defined in env

--- test/e2e.sh
+++ test/e2e.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 set -e
+
+# Override images.cfg if any of these env vars are set
+declare -a images=("LEADER_IMAGE" "PACKER_IMAGE" "PROXY_IMAGE")
+for i in "${images[@]}"
+do
+  # Substitute images if they're defined in the env
+  if [ "${!i}" != "" ]
+  then
+    sed -i '' "s@$i.*@$i=\"${!i}\"@" images.cfg
+  fi
+done
+
 ./cluster_up.sh imagertest
 ./cluster_down.sh imagertest
