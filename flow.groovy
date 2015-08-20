814be1180fc69aad290b08a150c01e816949e46d
Evan Brown <evanbrown@google.com>
2015-08-20 10:44:15 -0700

    Add flow.groovy for tests

---
+++ flow.groovy
@@ -0,0 +1,7 @@
+node('gcp-packer') {
+  git url: 'https://github.com/GoogleCloudPlatform/kube-jenkins-imager.git'
+  def app = docker.build 'e2e-kube-jenkins-imager'
+  app.withRun("-e 'LEADER_IMAGE=${LEADER_IMAGE}' -e 'PACKER_IMAGE=${PACKER_IMAGE}' -e 'PROXY_IMAGE=${PROXY_IMAGE}'") {c ->
+    sh "docker logs -f ${c.id}"
+  }
+}
