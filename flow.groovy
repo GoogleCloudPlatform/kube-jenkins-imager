54300024afdaa1f9221f38b5e8c2c8ca75656e82
Evan Brown <evanbrown@google.com>
2015-08-21 14:31:16 -0700

    parameterize git url

--- flow.groovy
+++ flow.groovy
@@ -1,6 +1,6 @@
 node('docker') {
-  git url: 'https://github.com/GoogleCloudPlatform/kube-jenkins-imager.git'
-  def app = docker.build 'e2e-kube-jenkins-imager'
+  def hash = git url: "${GIT_URL}"
+  def app = docker.build "${hash}"
   app.withRun("-e 'LEADER_IMAGE=${LEADER_IMAGE}' -e 'PACKER_IMAGE=${PACKER_IMAGE}' -e 'PROXY_IMAGE=${PROXY_IMAGE}'") {c ->
     sh "docker logs -f ${c.id}"
   }
