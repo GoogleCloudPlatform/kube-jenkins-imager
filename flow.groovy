bdeea2220d343d0b33df93a6f6cb974bef6797e4
Evan Brown <evanbrown@google.com>
2015-08-21 10:20:45 -0700

    Update build node

--- flow.groovy
+++ flow.groovy
@@ -1,4 +1,4 @@
-node('gcp-packer') {
+node('docker') {
   git url: 'https://github.com/GoogleCloudPlatform/kube-jenkins-imager.git'
   def app = docker.build 'e2e-kube-jenkins-imager'
   app.withRun("-e 'LEADER_IMAGE=${LEADER_IMAGE}' -e 'PACKER_IMAGE=${PACKER_IMAGE}' -e 'PROXY_IMAGE=${PROXY_IMAGE}'") {c ->
