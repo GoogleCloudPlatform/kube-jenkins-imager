6ba7bedf21d7c5e64866fe0357fabd351ea8a98f
Evan Brown <evandbrown@gmail.com>
2016-06-29 15:21:39 -0700

    Return exit code in Docker e2e tests

--- flow.groovy
+++ flow.groovy
@@ -1,7 +1,7 @@
 node('docker') {
-  def hash = git url: "${GIT_URL}"
-  def app = docker.build "${hash}"
-  app.withRun("-e 'LEADER_IMAGE=${LEADER_IMAGE}' -e 'PACKER_IMAGE=${PACKER_IMAGE}' -e 'PROXY_IMAGE=${PROXY_IMAGE}'") {c ->
-    sh "docker logs -f ${c.id}"
+  checkout scm
+  def app = docker.build "$env.BRANCH_NAME-$env.BUILD_NUMBER"
+  app.inside("-e 'LEADER_IMAGE=${LEADER_IMAGE}' -e 'PACKER_IMAGE=${PACKER_IMAGE}' -e 'PROXY_IMAGE=${PROXY_IMAGE}'") {
+    sh "./test/e2e.sh"
   }
 }
