92599b2d153e0328ed04a74c962012d8b668b7ef
J. Norment <jnorment@q2ebanking.com>
2016-08-01 19:12:41 -0500

    Dont commit the other git project; Small additions to cluster_up, more cleanup for cluster_down.

--- cluster_down.sh
+++ cluster_down.sh
@@ -12,7 +12,65 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 CLUSTER_NAME=${1-imager}
-ZONE=us-central1-f
+
+if [ "${CLUSTER_NAME}" = "imager" ]; then
+  CLUSTER_NAME='jenkins-imager'
+fi
+
+NETWORK_NAME="${CLUSTER_NAME}-net"
+
+ZONE=us-central1-c
 
 # Delete cluster
 gcloud container clusters delete --quiet ${CLUSTER_NAME} --zone ${ZONE}
+
+# Delete image
+# gcloud delete images jenkins-home-image
+
+# Delete persisted disk
+# gcloud compute disks delete jenkins-home
+
+# Delete firewall rules
+for rule in `gcloud compute firewall-rules list |grep ${NETWORK_NAME} | cut -d\  -f1`; do
+  gcloud compute firewall-rules delete $rule
+done
+
+ig="`gcloud compute instance-groups unmanaged list | grep ${NETWORK_NAME} | cut -d\  -f1`"
+uid="`echo $ig | cut -d\- -f4`"
+
+echo "$uid / $ig"
+
+# Delete forwarding rules
+for fr in `gcloud compute forwarding-rules list | grep $uid | cut -d\  -f1`; do
+  gcloud compute forwarding-rules delete $fr --global
+done
+
+# Delete target proxy 
+for tp in `gcloud compute target-http-proxies list | grep $uid | cut -d\  -f1`; do
+  gcloud compute target-http-proxies delete $tp
+done
+
+# Delete back ends
+for bes in `gcloud compute backend-services list | grep $ig | cut -d\  -f1`; do
+  gcloud compute backend-services delete $bes
+done
+
+# Delete instance groups
+for cig in `gcloud compute instance-groups unmanaged list $ig | cut -d\  -f1`; do
+  gcloud compute instance-groups unmanaged delete $cig
+done
+
+# Delete static compute addresses
+for caip in `gcloud compute addresses list | grep $uid | cut -d\  -f1`; do
+  gcloud compute addresses delete $caip --global
+done
+
+# Delete disk
+gcloud compute disks delete jenkins-home
+
+# Delete image
+gcloud compute images delete jenkins-home-image
+
+# Delete network
+gcloud compute networks delete ${NETWORK_NAME}
+
--- cluster_up.sh
+++ cluster_up.sh
@@ -17,17 +17,29 @@ function error_exit
 }
 
 # Check for cluster name as first (and only) arg
-CLUSTER_NAME=${1-imager}
+CLUSTER_NAME=${1-jenkins-imager}
+
+NETWORK_NAME="${CLUSTER_NAME}-net"
 NUM_NODES=3
 MACHINE_TYPE=n1-standard-1
-ZONE=us-central1-f
+ZONE=us-central1-c
 
 # Source the config
 . images.cfg
 
+if [ -d 'continuous-deployment-on-kubernetes' ]; then
+   echo
+   read -p "Note: path 'continuous-deployment-on-kubernetes' already exists. If this is a problem, stop the script now and delete it."
+   echo
+fi
+
+echo "* Creating network ${NETWORK_NAME} ..."
+gcloud compute networks create ${NETWORK_NAME} --mode auto
+
 echo "* Creating Google Container Engine cluster \"${CLUSTER_NAME}\"..."
 # Create cluster
 gcloud container clusters create ${CLUSTER_NAME} \
+  --network ${NETWORK_NAME} \
   --num-nodes ${NUM_NODES} \
   --machine-type ${MACHINE_TYPE} \
   --scopes "https://www.googleapis.com/auth/projecthosting,https://www.googleapis.com/auth/devstorage.full_control,https://www.googleapis.com/auth/monitoring,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/compute,https://www.googleapis.com/auth/cloud-platform" \
@@ -39,16 +51,20 @@ echo "* Configuring kubectl to use ${CLUSTER_NAME} cluster..."
 gcloud container clusters get-credentials ${CLUSTER_NAME}
 echo "done."
 
-echo "Getting Jenkins artifacts"
-git clone https://github.com/GoogleCloudPlatform/continuous-deployment-on-kubernetes
+if [ ! -d 'continuous-deployment-on-kubernetes' ]; then
+    echo "Getting Jenkins artifacts"
+    git clone https://github.com/GoogleCloudPlatform/continuous-deployment-on-kubernetes
+fi
 
 echo "Deploying Jenkins to Google Container Engine..."
 pushd continuous-deployment-on-kubernetes
+set +e
 echo "* Creating Jenkins home image"
 gcloud compute images create jenkins-home-image --source-uri https://storage.googleapis.com/solutions-public-assets/jenkins-cd/jenkins-home.tar.gz
 echo "* Creating Jenkins home disk"
 gcloud compute disks create jenkins-home --image jenkins-home-image --zone ${ZONE}
-PASSWORD=`openssl rand -base64 15`; echo "Your Jenkins password is $PASSWORD"; sed -i.bak s#CHANGE_ME#$PASSWORD# jenkins/k8s/options
+set -e
+PASSWORD=`openssl rand -base64 15`; echo "Your user password is $PASSWORD"; sed -i.bak s#CHANGE_ME#$PASSWORD# jenkins/k8s/options
 kubectl create ns jenkins
 kubectl create secret generic jenkins --from-file=jenkins/k8s/options --namespace=jenkins
 kubectl apply -f jenkins/k8s/
@@ -58,6 +74,12 @@ kubectl apply -f jenkins/k8s/lb/ingress.yaml
 popd
 echo "done."
 
+export NODE_PORT=$(kubectl get --namespace=jenkins -o jsonpath="{.spec.ports[0].nodePort}" services jenkins-ui)
+gcloud compute firewall-rules create allow-130-211-0-0-22 --source-ranges 130.211.0.0/22 --allow tcp:${NODE_PORT} --network ${NETWORK_NAME}
+gcloud compute firewall-rules create allow-ssh-${NETWORK_NAME} --source-ranges 0.0.0.0/0 --allow tcp:22 --network ${NETWORK_NAME}
+
+
 echo "All resources deployed."
 echo "Run 'kubectl get ingress jenkins --namespace jenkins -o \"jsonpath={.status.loadBalancer.ingress[0].ip}\";echo' to find your server's address, then give it a few minutes before trying to connect."
-echo "Login with user: jenkins and password ${PASSWORD}"
+echo "Login with user: jenkins and password: ${PASSWORD}"
+
