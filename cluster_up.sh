baa902428c05f61122a374064aa17a7f23aae79b
Vic Iglesias <viglesiasce@gmail.com>
2016-08-19 14:16:02 -0700

    Patch up scripts to be more robust (#24)

--- cluster_up.sh
+++ cluster_up.sh
@@ -25,31 +25,47 @@ ZONE=us-central1-f
 
 # Source the config
 . images.cfg
+if ! gcloud container clusters describe ${CLUSTER_NAME} > /dev/null 2>&1; then
+  echo "* Creating Google Container Engine cluster \"${CLUSTER_NAME}\"..."
+  # Create cluster
+  gcloud container clusters create ${CLUSTER_NAME} \
+    --num-nodes ${NUM_NODES} \
+    --machine-type ${MACHINE_TYPE} \
+    --scopes "https://www.googleapis.com/auth/projecthosting,https://www.googleapis.com/auth/devstorage.full_control,https://www.googleapis.com/auth/monitoring,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/compute,https://www.googleapis.com/auth/cloud-platform" \
+    --zone ${ZONE} \
+    --network ${NETWORK} || error_exit "Error creating Google Container Engine cluster"
+  echo "done."
+else
+  echo "* Google Container Engine cluster \"${CLUSTER_NAME}\" already exists..."
+fi
 
-echo "* Creating Google Container Engine cluster \"${CLUSTER_NAME}\"..."
-# Create cluster
-gcloud container clusters create ${CLUSTER_NAME} \
-  --num-nodes ${NUM_NODES} \
-  --machine-type ${MACHINE_TYPE} \
-  --scopes "https://www.googleapis.com/auth/projecthosting,https://www.googleapis.com/auth/devstorage.full_control,https://www.googleapis.com/auth/monitoring,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/compute,https://www.googleapis.com/auth/cloud-platform" \
-  --zone ${ZONE} \
-  --network ${NETWORK} || error_exit "Error creating Google Container Engine cluster"
-echo "done."
-
-# Make kubectl use new clusterc
+# Make kubectl use new cluster
 echo "* Configuring kubectl to use ${CLUSTER_NAME} cluster..."
 gcloud container clusters get-credentials ${CLUSTER_NAME} --zone ${ZONE}
 echo "done."
 
 echo "Getting Jenkins artifacts"
-git clone https://github.com/GoogleCloudPlatform/continuous-deployment-on-kubernetes
+if [ ! -d continuous-deployment-on-kubernetes ]; then
+  git clone https://github.com/GoogleCloudPlatform/continuous-deployment-on-kubernetes
+fi
 
 echo "Deploying Jenkins to Google Container Engine..."
 pushd continuous-deployment-on-kubernetes
-echo "* Creating Jenkins home image"
-gcloud compute images create jenkins-home-image --source-uri https://storage.googleapis.com/solutions-public-assets/jenkins-cd/jenkins-home.tar.gz
-echo "* Creating Jenkins home disk"
-gcloud compute disks create jenkins-home --image jenkins-home-image --zone ${ZONE}
+
+if ! gcloud compute images describe jenkins-home-image > /dev/null 2>&1; then
+  echo "* Creating Jenkins home image"
+  gcloud compute images create jenkins-home-image --source-uri https://storage.googleapis.com/solutions-public-assets/jenkins-cd/jenkins-home.tar.gz
+else
+  echo "* Jenkins home image already exists"
+fi
+
+if ! gcloud compute disks describe jenkins-home --zone ${ZONE} > /dev/null 2>&1; then
+  echo "* Creating Jenkins home disk"
+  gcloud compute disks create jenkins-home --image jenkins-home-image --zone ${ZONE}
+else
+  echo "* Jenkins home disk already exists"
+fi
+
 PASSWORD=`openssl rand -base64 15`; echo "Your Jenkins password is $PASSWORD"; sed -i.bak s#CHANGE_ME#$PASSWORD# jenkins/k8s/options
 kubectl create ns jenkins
 kubectl create secret generic jenkins --from-file=jenkins/k8s/options --namespace=jenkins
@@ -61,5 +77,7 @@ popd
 echo "done."
 
 echo "All resources deployed."
-echo "Run 'kubectl get ingress jenkins --namespace jenkins -o \"jsonpath={.status.loadBalancer.ingress[0].ip}\";echo' to find your server's address, then give it a few minutes before trying to connect."
+echo "In a few minutes your loadBalancer will finish provisioning. You can run the following to get its IP address:"
+echo "   kubectl get ingress jenkins --namespace jenkins -o \"jsonpath={.status.loadBalancer.ingress[0].ip}\";echo"
+echo
 echo "Login with user: jenkins and password ${PASSWORD}"
